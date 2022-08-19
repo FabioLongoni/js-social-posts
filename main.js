23163e90fc10a9701f8752b72937fd9a66fb945b
FabioLongoni <longoni.fabio90@gmail.com>
2022-08-19 11:27:55 +0200

    add-template

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ const posts = [
         "media": "https://unsplash.it/600/400?image=24",
         "author": {
             "name": "Luca Formicola",
-            "image": null
+            "image": "https://unsplash.it/300/300?image=24"
         },
         "likes": 56,
         "created": "2021-04-03"
@@ -55,3 +55,53 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+const postsNumber = [];
+
+for (key in posts) {
+    let authorPost = posts[key].author.name;
+    let contentPost = posts[key].content;
+    let imageProfile = posts[key].author.image;
+    let likesPost = posts[key].likes;
+    let contentCreated = posts[key].created;
+    let postMedia = posts[key].media;
+
+    let postsElements = `
+    <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${imageProfile}" alt="">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${authorPost}</div>
+                    <div class="post-meta__time">${contentCreated}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${contentPost}</div>
+        <div class="post__image">
+            <img src="${postMedia}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${likesPost}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>`
+    let containerElement = document.getElementById('container');
+    containerElement.innerHTML = postsElements;
+    console.log(containerElement);
+    
+    postsNumber.push(postsElements);
+    
+}
+
